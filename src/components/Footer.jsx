55c39f0550069d7636f410e2aa970b789ef1f598
Link0w0 <66062268+LinkHaggman@users.noreply.github.com>
2022-08-28 15:04:21 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hoyoverse from '../assets/Hoyoverse.png';
 import {
   FaFacebook,
   FaTwitter,
   FaInstagram,
-  FaPinterest,
+  FaDiscord,
   FaYoutube,
 } from 'react-icons/fa';
 
@@ -12,29 +13,24 @@ const Footer = () => {
     <div className='w-full bg-gray-100 py-16'>
       <div className='max-w-[1240px] mx-auto flex flex-col px-4'>
         <div className='sm:flex text-center justify-between items-center'>
-          <h1>BEACHES.</h1>
+        <img className='w-80' src={Hoyoverse} alt="/" />
           <div className='flex justify-between w-full sm:max-w-[280px] my-4'>
             <FaFacebook className='icon' />
             <FaTwitter className='icon' />
             <FaYoutube className='icon' />
-            <FaPinterest className='icon' />
+            <FaDiscord className='icon' />
             <FaInstagram className='icon' />
           </div>
         </div>
         <div className='flex justify-between'>
           <ul className='lg:flex'>
-            <li>About</li>
-            <li>Partnerships</li>
-            <li>Careers</li>
-            <li>Newsroom</li>
-            <li>Advertising</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
           </ul>
-          <ul className='text-right lg:flex'>
-            <li>Home</li>
-            <li>Destinations</li>
-            <li>Travel</li>
-            <li>View</li>
-            <li>Book</li>
+          <ul className='mt-3 text-right lg:flex'>
+         <p>Made with 🧡 by Link Häggman</p>
           </ul>
         </div>
       </div>
